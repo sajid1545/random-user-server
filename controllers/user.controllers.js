d9978e6ac0cff1ab83b6893d3ab489abed259a83
sajid1545 <clashking1545@gmail.com>
2023-06-26 21:24:39 +0600

    validation  added in addUser

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -31,6 +31,14 @@ const addUser = (req, res) => {
 			address,
 			photoURL,
 		};
+
+		// validation
+		if (!gender) return res.status(400).send({ success: false, message: 'gender is required' });
+		if (!name) return res.status(400).send({ success: false, message: 'name is required' });
+		if (!contact) return res.status(400).send({ success: false, message: 'contact is required' });
+		if (!address) return res.status(400).send({ success: false, message: 'address is required' });
+		if (!photoURL) return res.status(400).send({ success: false, message: 'photoURL is required' });
+
 		fs.writeFile('user.json', JSON.stringify([...userData, newUserInfo]), (err) => {
 			if (err) return res.status(500).send({ success: false, message: err });
 			res.status(200).send({ message: 'success', data: newUserInfo });
