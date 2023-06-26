88c31183bd95b7bac6add49df611f7224d38ee94
sajid1545 <clashking1545@gmail.com>
2023-06-26 22:38:57 +0600

    update singleUser handled

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -24,7 +24,6 @@ const addUser = (req, res) => {
 		if (err) return res.status(500).send({ success: false, message: err });
 		const userData = JSON.parse(data);
 		const { gender, name, contact, address, photoURL, id } = req.body;
-		
 
 		const newUserInfo = {
 			id: id,
@@ -54,10 +53,36 @@ const addUser = (req, res) => {
 };
 
 const updateSingleUser = (req, res) => {
-	const { id } = req.params.id;
-	if (!id) return res.status(400).send({ success: false, message: 'gender is required' });
-	console.log(id);
-	res.send(id);
+	const { id } = req.params;
+	if (isNaN(id)) return res.status(400).send({ success: false, message: 'id must be a number' });
+
+	fs.readFile('user.json', (err, data) => {
+		if (err) return res.status(500).send({ success: false, message: err });
+		let userData = JSON.parse(data.filter((user) => user.id !== id));
+		console.log(userData);
+		const updatedData = userData.find((user) => user.id === id);
+		if (!updatedData)
+			return res.status(400).send({ success: false, message: `user with id - ${id} not found` });
+
+		const { gender, name, contact, address, photoURL } = req.body;
+
+		const updatedDoc = {
+			id: id,
+			gender: gender || updatedData.gender,
+			name: name || updatedData.name,
+			contact: contact || updatedData.contact,
+			address: address || updatedData.address,
+			photoURL: photoURL || updatedData.photoURL,
+		};
+
+		const output = userData.filter((user) => user.id !== id);
+		const newData = [...output, updatedDoc];
+
+		fs.writeFile('user.json', JSON.stringify(newData), (err) => {
+			if (err) return res.status(500).send({ success: false, message: err });
+			res.status(200).send({ message: 'success' });
+		});
+	});
 };
 
 module.exports = {
--- routes/v1/user.routes.js
+++ routes/v1/user.routes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get('/all', userController.getAllUsers);
 router.get('/random', userController.getRandomUser);
 router.post('/save', userController.addUser);
-router.patch('/update', userController.updateSingleUser);
+router.patch('/update/:id', userController.updateSingleUser);
 
 module.exports = router;
